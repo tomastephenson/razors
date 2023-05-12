728cdf25437deeb9f0f11da05a8f2f7af4d8912d
Tom Stephenson <thomas.a.stephenson@gmail.com>
2023-05-13 00:04:36 +1200

    Revert "Change Homepage to edited components page"
    This reverts commit ef2603e4852ad0c37ca937122498d146227185f3.

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -2,30 +2,13 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
-import Content from '../components/Content.js'
-import Layout from '../components/Layout.js'
-import Accordion from '../components/Accordion'
-import BackgroundVideo from '../components/BackgroundVideo'
-import Gallery from '../components/Gallery'
-import Popup from '../components/Popup'
+import Content from '../components/Content'
+import Layout from '../components/Layout'
 
 // Export Template for use in CMS preview
-export const ComponentsPageTemplate = ({
-  title,
-  subtitle,
-  featuredImage,
-  section1,
-  section2,
-  video,
-  videoPoster,
-  videoTitle,
-  accordion,
-  body,
-  gallery
-}) => (
-  
+export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
   <main>
-    <PageHeader className="BackgroundVideo-section section">
+    <section className="BackgroundVideo-section section">
       <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
         {video && <source src={video} type="video/mp4" />}
       </BackgroundVideo>
@@ -33,51 +16,34 @@ export const ComponentsPageTemplate = ({
 
     <section className="section">
       <div className="container">
-        <Content source={section1} />
+        <Content source={body} />
       </div>
     </section>
-
-    <section className="section">
-      <div className="container">
-        <h2>Our gallery component</h2>
-        <Gallery images={gallery} />
-      </div>
-    </section>
-
   </main>
 )
 
-const ComponentsPage = ({ data: { page } }) => (
-  <Layout
-    meta={page.frontmatter.meta || false}
-    title={page.frontmatter.title || false}
-  >
-    <ComponentsPageTemplate {...page} {...page.frontmatter} body={page.html} />
+// Export Default HomePage for front-end
+const HomePage = ({ data: { page } }) => (
+  <Layout meta={page.frontmatter.meta || false}>
+    <HomePageTemplate {...page} {...page.frontmatter} body={page.html} />
   </Layout>
 )
 
-export default ComponentsPage
+export default HomePage
 
 export const pageQuery = graphql`
-  query ComponentsPage($id: String!) {
+  ## Query for HomePage data
+  ## Use GraphiQL interface (http://localhost:8000/___graphql)
+  ## $id is processed via gatsby-node.js
+  ## query name must be unique to this file
+  query HomePage($id: String!) {
     page: markdownRemark(id: { eq: $id }) {
       ...Meta
-      ...Gallery
       html
       frontmatter {
         title
-        template
         subtitle
         featuredImage
-        section1
-        section2
-        video
-        videoPoster
-        videoTitle
-        accordion {
-          title
-          description
-        }
       }
     }
   }
