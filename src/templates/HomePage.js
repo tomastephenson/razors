f4eca4f9531f73f0c5b783067e5b39a8e52c1819
Tom Stephenson <thomas.a.stephenson@gmail.com>
2023-05-12 23:45:19 +1200

    Update HomePage.js

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import PageHeader from '../components/PageHeader'
 import Content from '../components/Content.js'
 import Layout from '../components/Layout.js'
-import Accordion from '../components/Accordion'
 import BackgroundVideo from '../components/BackgroundVideo'
 import Gallery from '../components/Gallery'
-import Popup from '../components/Popup'
 
 // Export Template for use in CMS preview
 export const ComponentsPageTemplate = ({
-  title,
-  subtitle,
-  featuredImage,
   section1,
-  section2,
   video,
   videoPoster,
   videoTitle,
-  accordion,
-  body,
   gallery
 }) => (
   
@@ -65,18 +56,11 @@ export const pageQuery = graphql`
       ...Gallery
       html
       frontmatter {
-        title
-        template
-        subtitle
-        featuredImage
-        section1
-        section2
-        video
-        videoPoster
-        videoTitle
-        accordion {
-          title
-          description
+  section1,
+  video,
+  videoPoster,
+  videoTitle,
+  gallery
         }
       }
     }
