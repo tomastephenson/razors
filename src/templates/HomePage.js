14746ed510fa3e6a131596196da981ed91bcc7d3
Tom Stephenson <thomas.a.stephenson@gmail.com>
2023-05-13 00:04:29 +1200

    Revert "Update HomePage.js"
    This reverts commit f4eca4f9531f73f0c5b783067e5b39a8e52c1819.

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import PageHeader from '../components/PageHeader'
 import Content from '../components/Content.js'
 import Layout from '../components/Layout.js'
+import Accordion from '../components/Accordion'
 import BackgroundVideo from '../components/BackgroundVideo'
 import Gallery from '../components/Gallery'
+import Popup from '../components/Popup'
 
 // Export Template for use in CMS preview
 export const ComponentsPageTemplate = ({
+  title,
+  subtitle,
+  featuredImage,
   section1,
+  section2,
   video,
   videoPoster,
   videoTitle,
+  accordion,
+  body,
   gallery
 }) => (
   
@@ -56,11 +65,18 @@ export const pageQuery = graphql`
       ...Gallery
       html
       frontmatter {
-  section1,
-  video,
-  videoPoster,
-  videoTitle,
-  gallery
+        title
+        template
+        subtitle
+        featuredImage
+        section1
+        section2
+        video
+        videoPoster
+        videoTitle
+        accordion {
+          title
+          description
         }
       }
     }
