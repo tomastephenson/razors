19af24669946f4c2000941f71f45210e4d3ef5e2
Tom Stephenson <thomas.a.stephenson@gmail.com>
2023-05-13 00:04:09 +1200

    Revert "Restore Homepage"
    This reverts commit 947285625886fd3e9bd681aac10ff35d2b503ad0.

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -2,48 +2,105 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
-import Content from '../components/Content'
-import Layout from '../components/Layout'
+import Content from '../components/Content.js'
+import Layout from '../components/Layout.js'
+import Accordion from '../components/Accordion'
+import BackgroundVideo from '../components/BackgroundVideo'
+import Gallery from '../components/Gallery'
+import Popup from '../components/Popup'
 
 // Export Template for use in CMS preview
-export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
+export const ComponentsPageTemplate = ({
+  title,
+  subtitle,
+  featuredImage,
+  section1,
+  section2,
+  video,
+  videoPoster,
+  videoTitle,
+  accordion,
+  body,
+  gallery
+}) => (
   <main>
+    <PageHeader
+      title={title}
+      subtitle={subtitle}
+      backgroundImage={featuredImage}
+    />
+    <section className="section">
+      <div className="container">
+        <Content source={section1} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <h2>Our gallery component</h2>
+        <Gallery images={gallery} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <Content source={section2} />
+      </div>
+    </section>
+
     <section className="BackgroundVideo-section section">
       <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
         {video && <source src={video} type="video/mp4" />}
       </BackgroundVideo>
-    />
+    </section>
 
     <section className="section">
       <div className="container">
-        <Content source={body} />
+        <Accordion items={accordion} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <Popup>
+          <Content source={section1} />
+        </Popup>
       </div>
     </section>
   </main>
 )
 
-// Export Default HomePage for front-end
-const HomePage = ({ data: { page } }) => (
-  <Layout meta={page.frontmatter.meta || false}>
-    <HomePageTemplate {...page} {...page.frontmatter} body={page.html} />
+const ComponentsPage = ({ data: { page } }) => (
+  <Layout
+    meta={page.frontmatter.meta || false}
+    title={page.frontmatter.title || false}
+  >
+    <ComponentsPageTemplate {...page} {...page.frontmatter} body={page.html} />
   </Layout>
 )
 
-export default HomePage
+export default ComponentsPage
 
 export const pageQuery = graphql`
-  ## Query for HomePage data
-  ## Use GraphiQL interface (http://localhost:8000/___graphql)
-  ## $id is processed via gatsby-node.js
-  ## query name must be unique to this file
-  query HomePage($id: String!) {
+  query ComponentsPage($id: String!) {
     page: markdownRemark(id: { eq: $id }) {
       ...Meta
+      ...Gallery
       html
       frontmatter {
         title
+        template
         subtitle
         featuredImage
+        section1
+        section2
+        video
+        videoPoster
+        videoTitle
+        accordion {
+          title
+          description
+        }
       }
     }
   }
