5fea36d892343afcac5d4a5fd648cf53c4da9bdc
Tom Stephenson <thomas.a.stephenson@gmail.com>
2023-05-13 00:13:13 +1200

    Revert "Reverting the homepage content back so it can deploy properly!"
    This reverts commit ae575a3661d457616bf760541dc8cef5db49287a.

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -4,15 +4,16 @@ import { graphql } from 'gatsby'
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
+import BackgroundVideo from '../components/BackgroundVideo'
+
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
-  <main>
-    <section className="BackgroundVideo-section section">
+  
+  <main className="BackgroundVideo-section section">
       <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
         {video && <source src={video} type="video/mp4" />}
       </BackgroundVideo>
-    />
 
     <section className="section">
       <div className="container">
