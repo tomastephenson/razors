1e721f9a88a2d9472eb34371d8ef11376dd7844c
Tom Stephenson <thomas.a.stephenson@gmail.com>
2023-05-13 00:17:42 +1200

    Restore homepage.js to its original glory!

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -4,16 +4,16 @@ import { graphql } from 'gatsby'
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
-import BackgroundVideo from '../components/BackgroundVideo'
-
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
-  
-  <main className="BackgroundVideo-section section">
-      <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
-        {video && <source src={video} type="video/mp4" />}
-      </BackgroundVideo>
+  <main className="Home">
+    <PageHeader
+      large
+      title={title}
+      subtitle={subtitle}
+      backgroundImage={featuredImage}
+    />
 
     <section className="section">
       <div className="container">
